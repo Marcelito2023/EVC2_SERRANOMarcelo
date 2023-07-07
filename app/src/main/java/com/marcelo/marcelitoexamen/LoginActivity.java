233d246aafdc3b6ad54889507459fd10c824fc46
Marcelito2023 <MarcelitoSSJ@gmail.com>
2023-07-07 17:27:26 -0500

    marcelo

--- app/src/main/java/com/marcelo/marcelitoexamen/LoginActivity.java
+++ app/src/main/java/com/marcelo/marcelitoexamen/LoginActivity.java
@@ -2,13 +2,67 @@ package com.marcelo.marcelitoexamen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.marcelo.marcelitoexamen.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.tilemail.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+               boolean isOK= credenciales(s.toString(),binding.password.getEditText().getText().toString());
+                binding.btnLogin.setEnabled(isOK);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        binding.password.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                boolean isOK= credenciales(binding.tilemail.getEditText().getText().toString(),binding.password.getEditText().getText().toString());
+                binding.btnLogin.setEnabled(isOK);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
+    private boolean credenciales(String email, String password){
+
+        boolean isEmailOk = (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) && email.equals("ejemplo@idat.com.pe");
+        boolean isPasswordOk = (password.length() >= 8 ) && password.equals("Idat1234");
+        return isEmailOk && isPasswordOk;
+    }
+
 }
--- app/src/main/java/com/marcelo/marcelitoexamen/MainActivity.java
+++ app/src/main/java/com/marcelo/marcelitoexamen/MainActivity.java
@@ -5,16 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import java.util.Random;
+import com.google.android.material.snackbar.Snackbar;
+import com.marcelo.marcelitoexamen.databinding.ActivityMainBinding;
+
+
 
 public class MainActivity extends AppCompatActivity {
     private static final int delay = 3000; // 3 segundos
 
+    private ActivityMainBinding binding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -24,5 +32,24 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, delay);
+
+        binding.vector1.setOnClickListener(v -> {
+            String[] messages = {
+                    "Hello",
+                    "UGU",
+                    "klsdsds",
+                    "Csdsdsiao",
+                    "Holaotravez",
+
+            };
+
+            Random random = new Random();
+            int index = random.nextInt(messages.length);
+            String message = messages[index];
+
+            Snackbar.make(binding.getRoot(), message, Snackbar.LENGTH_SHORT).show();
+        });
     }
+
+
 }
